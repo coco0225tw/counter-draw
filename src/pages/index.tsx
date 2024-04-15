7268bcabcb08afe0868e809bbec1a48b173ae058
River <coco0958231520@gmail.com>
2024-04-15 23:40:31 +0800

    move folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { HomeWrapper } from '@/styles';
-import { Participant } from '../../components/Participant';
-import { Timer } from '../../components/Timer';
+import { Participant } from '../components/Participant';
+import { Timer } from '../components/Timer';
 
 export default function Home() {
   return (
